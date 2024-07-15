1f253776fe3b8d92f078f5d71b332fca061c5b96
1taroh <65810214+1taroh@users.noreply.github.com>
2024-07-15 16:55:38 +0900

    show run time

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import torchvision.transforms as transforms
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
+start = time.time()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # model = MLP()
@@ -25,10 +27,10 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
 testset = torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=8192, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=8192, shuffle=False)
 
-epochs = 5
+epochs = 10
 acc_list = []
 # モデルの学習
 for epoch in range(epochs):
@@ -38,5 +40,8 @@ for epoch in range(epochs):
     acc = model.test_loop(testloader, device)
     acc_list.extend(acc)
 
+end = time.time()
+
+print(f"time: {end-start:.2f}[sec]")
 plt.plot(acc_list)
 plt.show()
