904a31bd40a3739f6bb38c50d33ca1853e7d12da
1taroh <65810214+1taroh@users.noreply.github.com>
2024-07-15 16:01:48 +0900

    いろんなバッチサイズでベンチマーク

--- benchmark.py
+++ benchmark.py
@@ -12,29 +12,37 @@ def main():
     model_name = get_model_name()
     print(f"The selected model is: {model_name}")
 
-    print('Start Benchmark')
-    benchmark_results = benchmark(model_name)
-    print('End Benchmark')
+    train_batch_size_list = [64, 128, 256, 512, 1024]
+    train_batch_size_list = [64, 128, 256, 512, 1024]
 
     now = datetime.datetime.now()
     filename = now.strftime("%Y%m%d_%H%M%S_benchmark_results.txt")
     filepath = 'results/'+filename
-    #TODO: 様々なbatch_size でベンチマークしたい
-    save_benchmark_results(model_name, benchmark_results, filepath)
-    print('Benchmark results are saved.')
 
-def benchmark(model_name) -> list:
+    print('Start Benchmark')
+    for train_batch_size in train_batch_size_list:
+        for test_batch_size in train_batch_size_list:
+            benchmark_results = benchmark(model_name, train_batch_size, test_batch_size)
+            #TODO: 様々なbatch_size でベンチマークしたい
+            save_benchmark_results(model_name, benchmark_results, filepath)
+            print('Benchmark results are saved.')
+    print('End Benchmark')
+
+    print('All benchmark results are saved.')
+
+def benchmark(model_name, train_batch_size, test_batch_size) -> list:
+    epochs = 10
+
     if model_name == 'MLP':
-        benchmark_results = benchmark_MLP()
+        benchmark_results = benchmark_MLP(epochs, train_batch_size, test_batch_size)
     elif model_name == 'CNN':
-        benchmark_results = benchmark_CNN()
+        benchmark_results = benchmark_CNN(epochs, train_batch_size, test_batch_size)
     else:
         raise ValueError(f"Unknown model name: {model_name}. Expected 'MLP' or 'CNN'.")
         
-
     return benchmark_results
 
-def benchmark_MLP(epochs=3) -> list:
+def benchmark_MLP(epochs=3, train_batch_size=128, test_batch_size=256) -> list:
     start = time.time()
 
     ####### Start Benchmark ########
@@ -69,13 +77,18 @@ def benchmark_MLP(epochs=3) -> list:
 
     end = time.time()
     training_time = end - start
-    loss = 0
     accuracy = max(acc_list)
 
-    benchmark_results = [training_time, epochs, loss, accuracy]
+    benchmark_results = [
+        training_time,
+        epochs, 
+        train_batch_size,
+        test_batch_size,
+        accuracy
+        ]
     return benchmark_results
 
-def benchmark_CNN(epochs=3) -> list:
+def benchmark_CNN(epochs=3, train_batch_size=128, test_batch_size=256) -> list:
     start = time.time()
 
     ####### Start Benchmark ########
@@ -95,8 +108,8 @@ def benchmark_CNN(epochs=3) -> list:
     trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
     testset = torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batch_size, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=test_batch_size, shuffle=False)
 
     acc_list = []
     # モデルの学習
@@ -110,10 +123,15 @@ def benchmark_CNN(epochs=3) -> list:
 
     end = time.time()
     training_time = end - start
-    loss = 0
     accuracy = max(acc_list)
 
-    benchmark_results = [training_time, epochs, loss, accuracy]
+    benchmark_results = [
+        training_time,
+        epochs, 
+        train_batch_size,
+        test_batch_size,
+        accuracy
+        ]
     return benchmark_results
 
 def get_model_name() -> str:
@@ -126,10 +144,11 @@ def get_model_name() -> str:
     return model_name
 
 def save_benchmark_results(model_name, benchmark_results, filepath) -> None:
-    training_time = benchmark_results[0]
-    epochs        = benchmark_results[1]
-    loss          = benchmark_results[2]
-    accuracy      = benchmark_results[3]
+    training_time    = benchmark_results[0]
+    epochs           = benchmark_results[1]
+    train_batch_size = benchmark_results[2]
+    test_batch_size  = benchmark_results[3]
+    accuracy         = benchmark_results[4]
 
     with open(filepath, 'a') as f:
         f.write(torch.cuda.get_device_name())
@@ -137,7 +156,8 @@ def save_benchmark_results(model_name, benchmark_results, filepath) -> None:
         f.write(f'model name: {model_name}\n')
         f.write(f'Training time: {training_time:.2f}[sec]\n')
         f.write(f'Epochs: {epochs}\n')
-        f.write(f'Loss: {loss:.6f}\n')
+        f.write(f'Train Batch Size: {train_batch_size}\n')
+        f.write(f'Test Batch Size: {test_batch_size}\n')
         f.write(f'Accuracy: {accuracy:.2f}%\n')
 
 if __name__ == "__main__":
